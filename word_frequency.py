22f0fb3c5b786111ea641175618b6791b8105b2c
Gordon Fierce <gordon.fierce@gmail.com>
2015-01-15 00:23:53 -0500

    Made a fancy histogram and allowed a file argument

--- word_frequency.py
+++ word_frequency.py
@@ -1,10 +1,14 @@
+import sys
+
+
+
 
 def word_frequency(given_string):
 	"""Takes a string, returns a dictionary with the
 	count for each word in the string"""
 	word_dict = {}
 	clean_line = given_string
-	for character in "-!.,#*()\':;?/@:\"":
+	for character in "-!.,#*[]_()\':;?/@:\"":
 		clean_line = clean_line.replace(character," ")
 	clean_line = clean_line.lower()
 	our_words = clean_line.split()
@@ -12,26 +16,42 @@ def word_frequency(given_string):
 		word_dict[word] = (word_dict.get(word, 0) + 1)
 	return word_dict
 
+def gets_count(dictionary_item):
+	"""Returns the second item in a tuple"""
+	return dictionary_item[1]
 
-with open("sample.txt") as our_file:
+our_dict = {}
+
+default_text = "sample.txt"
+
+if len(sys.argv) > 1:
+	source_file = sys.argv[1]
+else:
+	source_file = default_text
+
+with open(source_file) as our_file:
 	our_text = our_file.readlines()
-	our_dict = {}
 	for our_line in our_text:
 		midway_dict = word_frequency(our_line)
 		for key in midway_dict:
 			our_dict[key] = midway_dict[key] + our_dict.get(key, 0)
 
-def gets_count(dictionary_item):
-	"""Returns the second item in a tuple"""
-	return dictionary_item[1]
-
 dict_list = our_dict.items()
 sorted_dict_list = sorted(dict_list, key=gets_count, reverse=True)
 top_twenty = sorted_dict_list[:20]
 
-for word, count in top_twenty:
-	print "{} {}".format(word, count)
+# for word, count in top_twenty:
+#	print("{} {}".format(word.ljust(10), count))
 
-def fancy_histogram(given_dict, count=20):
+def fancy_histogram(given_list, count=20):
 	"""Prints a normalized histogram to the terminal of the top 20 items in a
 	sorted list of word/count tuples"""
+	largest_count = given_list[0][1]
+	normalized_list = []
+	for item in range(count):
+		normalized_key_count = (50 * given_list[item][1]) // largest_count
+		normalized_list.append((given_list[item][0],"#"*normalized_key_count))
+	for pair in normalized_list:
+		print("{} {}".format(pair[0].ljust(10), pair[1]))
+
+fancy_histogram(sorted_dict_list)
